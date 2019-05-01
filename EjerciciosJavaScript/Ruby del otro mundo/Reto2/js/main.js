6b3f102e6169a4b96771cddee1eba64ab1109283
Dardila11 <danieleardila@hotmail.com>
2019-05-01 17:33:01 -0500

    Arreglado reto 2 Ruby
    incrementa de uno a uno

--- EjerciciosJavaScript/Ruby del otro mundo/Reto2/js/main.js
+++ EjerciciosJavaScript/Ruby del otro mundo/Reto2/js/main.js
@@ -1,16 +1,23 @@
-
+var contador = 0;
 
 window.onkeydown = function checkKey(key){
-    var years = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019];
-    var item = years[Math.floor(Math.random()*years.length)];
+    var year = document.getElementById("year").innerHTML;
+    var numYear = parseInt(year,10);
     if(key.keyCode == 38){ // codigo de la tecla "up"
-        
-        if(item == 2019){
-            document.getElementById("futuro").innerHTML = "Esta noticia es del futuro!"
+        numYear = numYear + 1;
+        console.log(numYear);
+        if(numYear > 2010){
+            contador++;
+            console.log(contador);
+            numYear = numYear + contador - 1;
+            if(numYear == 2019){
+                document.getElementById("futuro").innerHTML = "Esta noticia es del futuro!"
+                document.getElementById("year").innerHTML = numYear;
+            }
         }
         else{
             document.getElementById("futuro").innerHTML = ""
+            document.getElementById("year").innerHTML = numYear;
         }
-        document.getElementById("year").innerHTML = item;
     }
 }

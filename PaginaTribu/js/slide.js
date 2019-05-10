619380a0196633cd5d77e0667e84fda828963565
Dardila11 <danieleardila@hotmail.com>
2019-05-09 21:44:46 -0500

    pagina personal terminada

--- PaginaTribu/js/slide.js
+++ PaginaTribu/js/slide.js
@@ -1,7 +1,12 @@
 var slideIndex = 1;
 var slideIndexV = 1;
-showSlides(slideIndex);
-showSlidesV(slideIndexV);
+/*showSlides(slideIndex);
+showSlidesV(slideIndexV);*/
+
+window.onload = function loadSlides(){
+    showSlides(slideIndex);
+    showSlidesV(slideIndexV);
+}
 
 
 // Next/previous controls
@@ -19,37 +24,27 @@ function currentSlide(n) {
 }
 
 function currentSlideV(n) {
-    showSlidesV(slideIndex = n);
+    showSlidesV(slideIndexV = n);
   }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  //var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
-  /*for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }*/
   slides[slideIndex-1].style.display = "block"; 
-  //dots[slideIndex-1].className += " active";
 }
 
 function showSlidesV(n) {
     var i;
     var slides = document.getElementsByClassName("mySlidesV");
-    //var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndexV = 1} 
     if (n < 1) {slideIndexV = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; 
     }
-    /*for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }*/
     slides[slideIndexV-1].style.display = "block"; 
-    //dots[slideIndex-1].className += " active";
   }

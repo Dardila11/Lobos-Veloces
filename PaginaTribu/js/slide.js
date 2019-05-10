39238d7d614f5ffeb5913011a950446e0031b567
Dardila11 <danieleardila@hotmail.com>
2019-05-09 20:32:33 -0500

    slideshows funcionando independientemente

--- PaginaTribu/js/slide.js
+++ PaginaTribu/js/slide.js
@@ -1,5 +1,7 @@
 var slideIndex = 1;
+var slideIndexV = 1;
 showSlides(slideIndex);
+showSlidesV(slideIndexV);
 
 
 // Next/previous controls
@@ -7,10 +9,18 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+function plusSlidesV(n) {
+    showSlidesV(slideIndexV += n);
+}
+
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+
+function currentSlideV(n) {
+    showSlidesV(slideIndex = n);
+  }
 
 function showSlides(n) {
   var i;
@@ -27,3 +37,19 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   //dots[slideIndex-1].className += " active";
 }
+
+function showSlidesV(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlidesV");
+    //var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndexV = 1} 
+    if (n < 1) {slideIndexV = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    /*for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }*/
+    slides[slideIndexV-1].style.display = "block"; 
+    //dots[slideIndex-1].className += " active";
+  }
